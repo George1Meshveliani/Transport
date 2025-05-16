57b63644714776c6c6bd3e3fae009ac286836a17
George Meshveliani <giorgi.meshveliani@gmail.com>
2025-05-16 19:14:23 +0400

    Update Transport.h

--- Transport.h
+++ Transport.h
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+class Transport {
+    public:
+        int price;
+        int speed;
+        int numberOfPassengers;
+
+        Transport(int p, int s, int n) {
+            price = p;
+            speed = s;
+            numberOfPassengers = n;
+        }
+
+        void destination(string d);
+};
+
+void Transport::destination(string d) {
+    cout << d << endl;
+}
